4b038990c01ea5501b7b43bde096c5e21ed9359a
Ivan Sanchez <ivansanchez.uam@gmail.com>
2017-02-26 18:29:49 -0600

    Pantalla principal terminada con TableLayout.

--- app/src/main/java/com/losjuanes/ordernowapp/presentacion/vistas/PrincipalActivity.java
+++ app/src/main/java/com/losjuanes/ordernowapp/presentacion/vistas/PrincipalActivity.java
@@ -1,28 +1,77 @@
 package com.losjuanes.ordernowapp.presentacion.vistas;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.losjuanes.ordernowapp.R;
 
-public class PrincipalActivity extends AppCompatActivity {
-    TextView tvNumOrden;
+public class PrincipalActivity extends AppCompatActivity implements View.OnClickListener{
     int numOrden;
+
+    RelativeLayout rl_conocenos, rl_menu, rl_orden,
+                    rl_necesitasAlgo, rl_sucursales, rl_quejasYsugerencias;
+
+    ControladorMuestraMenu controladorMenu;
+
+    Activity principalActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        tvNumOrden = (TextView) findViewById(R.id.tvNumOrden);
+        controladorMenu = new ImplementacionControladorMuestraMenu();
 
-        //Obtenemos los datos que envia la actividad NumPersonasActivity
+        //Obtenemos el numero de orden que envia la actividad NumPersonasActivity
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
 
         numOrden = bundle.getInt("numOrden");
-        tvNumOrden.setText("Orden: " + numOrden);
+
+        rl_conocenos = (RelativeLayout) findViewById(R.id.rl_conocenos);
+        rl_menu = (RelativeLayout) findViewById(R.id.rl_menu);
+        rl_orden = (RelativeLayout) findViewById(R.id.rl_orden);
+        rl_necesitasAlgo = (RelativeLayout) findViewById(R.id.rl_necesitasAlgo);
+        rl_sucursales = (RelativeLayout) findViewById(R.id.rl_sucursales);
+        rl_quejasYsugerencias = (RelativeLayout) findViewById(R.id.rl_quejasYsugerencias);
+
+        rl_conocenos.setOnClickListener(this);
+        rl_menu.setOnClickListener(this);
+        rl_orden.setOnClickListener(this);
+        rl_necesitasAlgo.setOnClickListener(this);
+        rl_sucursales.setOnClickListener(this);
+        rl_quejasYsugerencias.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.rl_conocenos:
+                Toast.makeText(principalActivity, "1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_menu:
+                Toast.makeText(principalActivity, "2", Toast.LENGTH_SHORT).show();
+                controladorMenu.iniciaActivity(principalActivity, MenuActivity.class);
+                break;
+            case R.id.rl_orden:
+                Toast.makeText(principalActivity, "3", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_necesitasAlgo:
+                Toast.makeText(principalActivity, "4", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_sucursales:
+                Toast.makeText(principalActivity, "5", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_quejasYsugerencias:
+                Toast.makeText(principalActivity, "6", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
